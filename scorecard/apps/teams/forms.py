57b828470267e0288040e0ff7b4290a6aa5f2ecd
Songqing Liu <sliu@west.com>
2015-11-20 11:33:09 -0600

    test metrics detail/edit for QA, TE, PQ is working

--- scorecard/apps/teams/forms.py
+++ scorecard/apps/teams/forms.py
@@ -1,18 +1,9 @@
 from django import forms
 from django.forms import ModelForm
 from models import TestMetrics, InnovationMetrics, LabMetrics, RequirementMetrics
-from scorecard.apps.users.models import FunctionalGroup
 
 
 class InnovationForm(ModelForm):
-    def __init__(self, *args, **kwargs):
-        super(InnovationForm, self).__init__(*args, **kwargs)
-        try:
-            qi = FunctionalGroup.objects.get(key='QI')
-            self.fields['functional_group'].initial = qi.id
-        except FunctionalGroup.DoesNotExist:
-            self.fields['functional_group'].initial = None
-
     class Meta:
         model = InnovationMetrics
         exclude = ['created', 'confirmed']
@@ -25,14 +16,6 @@ class InnovationForm(ModelForm):
 
 
 class LabForm(ModelForm):
-    def __init__(self, *args, **kwargs):
-        super(LabForm, self).__init__(*args, **kwargs)
-        try:
-            tl = FunctionalGroup.objects.get(key='TL')
-            self.fields['functional_group'].initial = tl.id
-        except FunctionalGroup.DoesNotExist:
-            self.fields['functional_group'].initial = None
-
     class Meta:
         model = LabMetrics
         fields = ['functional_group', 'staffs', 'openings', 'contractors',
@@ -48,14 +31,6 @@ class LabForm(ModelForm):
 
 
 class RequirementForm(ModelForm):
-    def __init__(self, *args, **kwargs):
-        super(RequirementForm, self).__init__(*args, **kwargs)
-        try:
-            re = FunctionalGroup.objects.get(key='RE')
-            self.fields['functional_group'].initial = re.id
-        except FunctionalGroup.DoesNotExist:
-            self.fields['functional_group'].initial = None
-
     class Meta:
         model = RequirementMetrics
         fields = ['functional_group', 'staffs', 'openings', 'contractors',
@@ -73,14 +48,6 @@ class RequirementForm(ModelForm):
 
 
 class TestForm(ModelForm):
-    # def __init__(self, *args, **kwargs):
-    #     super(TestForm, self).__init__(*args, **kwargs)
-    #     try:
-    #         pq = FunctionalGroup.objects.get(key='PQ')
-    #         self.fields['functional_group'].initial = pq.id
-    #     except FunctionalGroup.DoesNotExist:
-    #         self.fields['functional_group'].initial = None
-
     class Meta:
         model = TestMetrics
         exclude = ['created', 'confirmed']
