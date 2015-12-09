2febf0555bac108b3f40057984cfe5a2c891f053
Songqing Liu <sliu@west.com>
2015-12-09 10:03:58 -0600

    try use gmail smtp server for qaci01

--- scorecard/settings/local.py
+++ scorecard/settings/local.py
@@ -8,3 +8,10 @@ ALLOWED_HOSTS = ['*']
 # STATIC_URL = 'http://10.6.20.109/static/'
 
 HOST_URL = 'http://10.6.20.109:8000' + LOGIN_URL
+
+#----------------GMAIL Backend----------------
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'ceeqwic@gmail.com'
+EMAIL_HOST_PASSWORD = '^S=+c3gyYu6F74D'
+EMAIL_PORT = 587
