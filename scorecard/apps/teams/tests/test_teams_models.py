eb958252f4b2c47b44c16d407e505234af2fe538
Songqing Liu <sliu@west.com>
2015-12-10 15:05:52 -0600

    continue adding unit tests

--- scorecard/apps/teams/tests/test_teams_models.py
+++ scorecard/apps/teams/tests/test_teams_models.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import random
 from django.test import TestCase
 from django.utils.timezone import localtime
 import pytz
@@ -17,10 +18,10 @@ class InnovationMetricsModelTest(TestCase):
             created=datetime.utcnow().replace(tzinfo=pytz.utc),
             confirmed=datetime.utcnow().replace(tzinfo=pytz.utc)
         )
-        self.staffs = 2
-        self.contractors = 1
-        self.story_points_execution = 32
-        self.license_cost = 135.65
+        self.staffs = random.randint(1, 100)
+        self.contractors = random.randint(1, 100)
+        self.story_points_execution = random.random() * 100
+        self.license_cost = random.random() * 100
 
     def test_string_representations(self):
         self.assertEqual(str(self.innovation), '{0}: {1}: {2}'.format(self.innovation.functional_group.name,
@@ -37,12 +38,14 @@ class InnovationMetricsModelTest(TestCase):
         self.innovation.staffs = self.staffs
         self.innovation.story_points_execution = self.story_points_execution
         self.innovation.save()
+
         self.assertEqual(self.innovation.avg_throughput, self.story_points_execution / self.staffs)
 
     def test_operational_cost(self):
         self.innovation.staffs = self.staffs
         self.innovation.contractors = self.contractors
         self.innovation.save()
+
         self.assertEqual(self.innovation.operational_cost, (self.staffs + self.contractors) * 40 * 45)
 
     def test_total_operational_cost(self):
@@ -50,8 +53,95 @@ class InnovationMetricsModelTest(TestCase):
         self.innovation.contractors = self.contractors
         self.innovation.license_cost = self.license_cost
         self.innovation.save()
+
         self.assertEqual(self.innovation.total_operational_cost,
                          self.innovation.operational_cost + self.license_cost
                          )
 
+
+class RequirementMetricsModelTest(TestCase):
+    def setUp(self):
+        self.functional_group = FunctionalGroup.objects.create(
+            name='Quality Innovation',
+            key='QI'
+        )
+        self.requirement = RequirementMetrics.objects.create(
+            functional_group=self.functional_group,
+            created=datetime.utcnow().replace(tzinfo=pytz.utc),
+            confirmed=datetime.utcnow().replace(tzinfo=pytz.utc)
+        )
+        self.staffs = random.randint(1, 100)
+        self.contractors = random.randint(1, 100)
+        self.active_projects = random.randint(1, 100)
+        self.team_initiative = random.randint(1, 100)
+        self.elicitation_analysis_time = random.random() * 100
+        self.rework_external_time = random.random() * 100
+        self.license_cost = random.random() * 100
+        self.travel_cost = random.random() * 100
+
+    def test_string_representations(self):
+        self.assertEqual(str(self.requirement), '{0}: {1}: {2}'.format(self.requirement.functional_group.name,
+                                                                       localtime(self.requirement.confirmed),
+                                                                       localtime(self.requirement.created)))
+
+    def test_verbose_name_plural(self):
+        self.assertEqual(str(RequirementMetrics._meta.verbose_name_plural), 'requirement metricss')
+
+    def test_avg_throughput(self):
+        self.requirement.active_projects = self.active_projects
+        self.requirement.team_initiative = self.team_initiative
+        self.requirement.save()
+
+        self.assertEqual(self.requirement.avg_throughput,
+                         self.active_projects + self.team_initiative)
+
+    def test_gross_available_time(self):
+        self.requirement.staffs = self.staffs
+        self.requirement.save()
+
+        self.assertEqual(self.requirement.gross_available_time, self.staffs * 6 * 5)
+
+    def test_efficiency_no_staffs(self):
+        self.assertEqual(self.requirement.efficiency, 0)
+
+    def test_efficiency(self):
+        self.requirement.staffs = self.staffs
+        self.requirement.elicitation_analysis_time = self.elicitation_analysis_time
+        self.requirement.save()
+
+        self.assertEqual(self.requirement.efficiency, self.elicitation_analysis_time / self.requirement.gross_available_time)
+
+    def test_rework_external_cost(self):
+        self.requirement.rework_external_time = self.rework_external_time
+        self.requirement.save()
+
+        self.assertEqual(self.requirement.rework_external_cost,
+                         self.rework_external_time * 50)
+
+    def test_operational_cost(self):
+        self.requirement.staffs = self.staffs
+        self.requirement.contractors = self.contractors
+        self.requirement.save()
+
+        self.assertEqual(self.requirement.operational_cost,
+                         (self.staffs + self.contractors) * 30 * 50)
+
+    def test_total_operational_cost(self):
+        self.requirement.staffs = self.staffs
+        self.requirement.contractors = self.contractors
+        self.requirement.license_cost = self.license_cost
+        self.requirement.save()
+
+        self.assertEqual(self.requirement.total_operational_cost,
+                         self.requirement.operational_cost + self.license_cost)
+
+    def test_overall_cost(self):
+        self.requirement.staffs = self.staffs
+        self.requirement.contractors = self.contractors
+        self.requirement.license_cost = self.license_cost
+        self.requirement.travel_cost = self.travel_cost
+        self.requirement.save()
+
+        self.assertEqual(self.requirement.overall_cost,
+                         self.requirement.total_operational_cost + self.travel_cost)
 
--- scorecard/apps/users/tests/test_users_models.py
+++ scorecard/apps/users/tests/test_users_models.py
@@ -1,4 +1,38 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
 from scorecard.apps.users.models import FunctionalGroup, HumanResource
 
+
+class FunctionalGroupModelTest(TestCase):
+    def test_string_representations(self):
+        functional_group = FunctionalGroup.objects.create(
+            name='Test Team Engineering',
+            key='TTE'
+        )
+        self.assertEqual(str(functional_group), '{0}: {1}'.format(functional_group.name, functional_group.key))
+
+    def test_verbose_name_plural(self):
+        self.assertEqual(str(FunctionalGroup._meta.verbose_name_plural), 'functional groups')
+
+
+class HumanResourceModelTest(TestCase):
+    def setUp(self):
+        self.functional_group = FunctionalGroup.objects.create(
+            name='Test Team Engineering',
+            key='TTE'
+        )
+        self.user = User.objects.create(
+            username='Test User',
+            password='Password'
+        )
+
+    def test_string_representations(self):
+        human = HumanResource.objects.create(
+            functional_group=self.functional_group,
+            user=self.user
+        )
+        self.assertEqual(str(human), '{0}: {1}'.format(human.user.username, human.manager))
+
+    def test_verbose_name_plural(self):
+        self.assertEqual(str(HumanResource._meta.verbose_name_plural), 'human resources')
