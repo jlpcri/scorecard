3917ae7a15f1090a2755f28ae608efb170b73f4b
Songqing Liu <sliu@west.com>
2015-12-15 13:34:18 -0600

    RBAC only manager can access to its team data input

--- scorecard/apps/teams/templates/teams/teams.js
+++ scorecard/apps/teams/templates/teams/teams.js
@@ -1,7 +1,8 @@
 /**
  * Created by sliu on 11/17/15.
  */
-var active_tab = String("");
+var active_tab = String(""),
+    key = '{{user.humanresource.functional_group.key}}';
 
 $('#subnav-tabs').find('a[data-toggle="tab"]').on('show.bs.tab', function(e){
     active_tab = e.target.hash;
@@ -9,6 +10,27 @@ $('#subnav-tabs').find('a[data-toggle="tab"]').on('show.bs.tab', function(e){
 
 
 $(document).ready(function(){
-    $('#subnav-tabs').find('a[href="#product_quality"]').tab('show');
+    switch (key) {
+        case 'PQ':
+            $('#subnav-tabs').find('a[href="#product_quality"]').tab('show');
+            break;
+        case 'QA':
+            $('#subnav-tabs').find('a[href="#quality_assurance"]').tab('show');
+            break
+        case 'QI':
+            $('#subnav-tabs').find('a[href="#quality_innovation"]').tab('show');
+            break;
+        case 'RE':
+            $('#subnav-tabs').find('a[href="#requirements_engineering"]').tab('show');
+            break;
+        case 'TE':
+            $('#subnav-tabs').find('a[href="#test_engineering"]').tab('show');
+            break;
+        case 'TL':
+            $('#subnav-tabs').find('a[href="#test_lab"]').tab('show');
+            break;
+        default :
+            $('#subnav-tabs').find('a[href="#product_quality"]').tab('show');
+    }
 });
 
