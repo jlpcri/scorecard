dc2d160570534cea1c368ddebf3b027d66e581cd
Songqing Liu <sliu@west.com>
2015-12-23 11:39:32 -0600

    update settings for laptop

--- scorecard/settings/local.py
+++ scorecard/settings/local.py
@@ -1,3 +1,4 @@
+import socket
 from base import *
 
 DEBUG = True
@@ -11,15 +12,15 @@ ALLOWED_HOSTS = ['*']
 # HOST_URL = 'http://10.6.20.109:8000' + LOGIN_URL
 HOST_URL = 'http://apps.qaci01.wic.west.com' + LOGIN_URL
 
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'scorecard',
-        'USER': 'scorecard',
-        'PASSWORD': 'scorecard_development',
-        'HOST': 'qaci01.wic.west.com',
-        # 'PORT': '5432',
-        'PORT': '5433'  # another postgres instance
+if socket.gethostname() == 'sliu-OptiPlex-GX520':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'scorecard',
+            'USER': 'scorecard',
+            'PASSWORD': 'scorecard_development',
+            'HOST': 'qaci01.wic.west.com',
+            # 'PORT': '5432',
+            'PORT': '5433'  # another postgres instance
+        }
     }
-}
