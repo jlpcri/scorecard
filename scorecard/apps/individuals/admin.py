92919a9024cfc2df27551e6e62ba2cdec95a9973
Songqing Liu <sliu@west.com>
2015-12-30 15:46:47 -0600

    continue working on individual scorecard model design etc.

--- scorecard/apps/individuals/admin.py
+++ scorecard/apps/individuals/admin.py
@@ -3,12 +3,14 @@ from django.contrib import admin
 from models import ProductQualityIndividual, PQProjects, PQInitiatives,\
     QualityAssuranceIndividual, QAProjects, QAInitiatives,\
     TestEngineeringIndividual, TEProjects, TEInitiatives,\
-    RequirementsEngineeringIndividual, QualityInnovationIndividual, TestLabIndividual
+    RequirementsEngineeringIndividual, QualityInnovationIndividual, TestLabIndividual,\
+    Projects, DefiningObjectives
 
 
 for m in [ProductQualityIndividual, PQProjects, PQInitiatives,
           QualityAssuranceIndividual, QAProjects, QAInitiatives,
           TestEngineeringIndividual, TEProjects, TEInitiatives,
-          RequirementsEngineeringIndividual, QualityInnovationIndividual, TestLabIndividual
+          # RequirementsEngineeringIndividual, QualityInnovationIndividual, TestLabIndividual,
+          Projects, DefiningObjectives
           ]:
     admin.site.register(m)
--- scorecard/apps/individuals/models.py
+++ scorecard/apps/individuals/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.timezone import localtime
 
 from scorecard.apps.users.models import FunctionalGroup, HumanResource
 
@@ -19,13 +20,20 @@ class Projects(models.Model):
 
 class DefiningObjectives(models.Model):
     name = models.CharField(max_length=50, default='')
+    functional_group = models.ForeignKey(FunctionalGroup, default=None)
+
+    class Meta:
+        unique_together = ('name', 'functional_group', )
+
+    def __unicode__(self):
+        return '{0}: {1}'.format(self.name, self.functional_group.key)
 
 
 class BaseIndividual(models.Model):
     """
     Base Individual for all teams
     """
-    human_resource = models.ForeignKey(HumanResource)
+    # human_resource = models.ForeignKey(HumanResource)
     created = models.DateTimeField(auto_now_add=True, db_index=True)
     confirmed = models.DateTimeField(auto_now=True, db_index=True)
     updated = models.BooleanField(default=False)
@@ -39,27 +47,48 @@ class BaseIndividual(models.Model):
 
 
 class ProductQualityIndividual(BaseIndividual):
-    pass
+    human_resource = models.ForeignKey(HumanResource,
+                                       limit_choices_to={'functional_group__key': 'PQ'})
+
+    def __unicode__(self):
+        return '{0}: {1}: {2}'.format(self.human_resource.user,
+                                      self.human_resource.functional_group.key,
+                                      localtime(self.created))
 
 
 class QualityAssuranceIndividual(BaseIndividual):
-    pass
+    human_resource = models.ForeignKey(HumanResource,
+                                       limit_choices_to={'functional_group__key': 'QA'})
+
+    def __unicode__(self):
+        return '{0}: {1}: {2}'.format(self.human_resource.user,
+                                      self.human_resource.functional_group.key,
+                                      localtime(self.created))
 
 
 class TestEngineeringIndividual(BaseIndividual):
-    pass
+    human_resource = models.ForeignKey(HumanResource,
+                                       limit_choices_to={'functional_group__key': 'TE'})
+
+    def __unicode__(self):
+        return '{0}: {1}: {2}'.format(self.human_resource.user,
+                                      self.human_resource.functional_group.key,
+                                      localtime(self.created))
 
 
 class QualityInnovationIndividual(BaseIndividual):
-    pass
+    human_resource = models.ForeignKey(HumanResource,
+                                       limit_choices_to={'functional_group__key': 'QI'})
 
 
 class RequirementsEngineeringIndividual(BaseIndividual):
-    pass
+    human_resource = models.ForeignKey(HumanResource,
+                                       limit_choices_to={'functional_group__key': 'RE'})
 
 
 class TestLabIndividual(BaseIndividual):
-    pass
+    human_resource = models.ForeignKey(HumanResource,
+                                       limit_choices_to={'functional_group__key': 'TL'})
 
 
 class BaseTestInitiativesIndividual(models.Model):
@@ -109,32 +138,68 @@ class BaseTestProjectsIndividual(models.Model):
 
 class PQInitiatives(BaseTestInitiativesIndividual):
     pq_individual = models.ForeignKey(ProductQualityIndividual)
-    defining_objective = models.ForeignKey(DefiningObjectives)
+    defining_objective = models.ForeignKey(DefiningObjectives,
+                                           limit_choices_to={'functional_group__key': 'PQ'})
+
+    def __unicode__(self):
+        return '{0}: {1}: {2}'.format(self.pq_individual.human_resource.user,
+                                      self.defining_objective.name,
+                                      localtime(self.pq_individual.created))
 
 
 class PQProjects(BaseTestProjectsIndividual):
     pq_individual = models.ForeignKey(ProductQualityIndividual)
-    project = models.ForeignKey(Projects)
+    project = models.ForeignKey(Projects,
+                                limit_choices_to={'functional_group__key': 'PQ'})
+
+    def __unicode__(self):
+        return '{0}: {1}: {2}'.format(self.pq_individual.human_resource.user,
+                                      self.project.name,
+                                      localtime(self.pq_individual.created))
 
 
 class QAInitiatives(BaseTestInitiativesIndividual):
     qa_individual = models.ForeignKey(QualityAssuranceIndividual)
-    defining_objective = models.ForeignKey(DefiningObjectives)
+    defining_objective = models.ForeignKey(DefiningObjectives,
+                                           limit_choices_to={'functional_group__key': 'QA'})
+
+    def __unicode__(self):
+        return '{0}: {1}: {2}'.format(self.qa_individual.human_resource.user,
+                                      self.defining_objective.name,
+                                      localtime(self.qa_individual.created))
 
 
 class QAProjects(BaseTestProjectsIndividual):
     qa_individual = models.ForeignKey(QualityAssuranceIndividual)
-    project = models.ForeignKey(Projects)
+    project = models.ForeignKey(Projects,
+                                limit_choices_to={'functional_group__key': 'QA'})
 
     quality_engineering_days_late = models.PositiveIntegerField(default=0)
     test_data_request_days_late = models.PositiveIntegerField(default=0)
 
+    def __unicode__(self):
+        return '{0}: {1}: {2}'.format(self.qa_individual.human_resource.user,
+                                      self.project.name,
+                                      localtime(self.qa_individual.created))
+
 
 class TEInitiatives(BaseTestInitiativesIndividual):
     te_individual = models.ForeignKey(TestEngineeringIndividual)
-    defining_objective = models.ForeignKey(DefiningObjectives)
+    defining_objective = models.ForeignKey(DefiningObjectives,
+                                           limit_choices_to={'functional_group__key': 'TE'})
+
+    def __unicode__(self):
+        return '{0}: {1}: {2}'.format(self.te_individual.human_resource.user,
+                                      self.defining_objective.name,
+                                      localtime(self.te_individual.created))
 
 
 class TEProjects(BaseTestProjectsIndividual):
     te_individual = models.ForeignKey(TestEngineeringIndividual)
-    project = models.ForeignKey(Projects)
+    project = models.ForeignKey(Projects,
+                                limit_choices_to={'functional_group__key': 'TE'})
+
+    def __unicode__(self):
+        return '{0}: {1}: {2}'.format(self.te_individual.human_resource.user,
+                                      self.project.name,
+                                      localtime(self.te_individual.created))
