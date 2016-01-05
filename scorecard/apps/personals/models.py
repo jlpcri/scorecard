9fd76380b5208c890a8acc32e7ef3d9699bb8b0a
Songqing Liu <sliu@west.com>
2016-01-05 15:05:47 -0600

    update personal models design

--- scorecard/apps/personals/models.py
+++ scorecard/apps/personals/models.py
@@ -4,44 +4,92 @@ from django.utils.timezone import localtime
 from scorecard.apps.users.models import FunctionalGroup, HumanResource
 
 
-class InnovationStats(models.Model):
+class BaseStats(models.Model):
+    """
+    Base Individual Status for all teams
+    """
+    human_resource = models.ForeignKey(HumanResource)
+    created = models.DateTimeField(auto_now_add=True, db_index=True)
+    confirmed = models.DateTimeField(auto_now=True, db_index=True)
+    updated = models.BooleanField(default=False)
+
+    compliments = models.PositiveIntegerField(default=0)
+    complaints = models.PositiveIntegerField(default=0)
+    escalations = models.PositiveIntegerField(default=0)
+
+    # Efficiency
+    overtime_weekday = models.DecimalField(max_digits=10, decimal_places=2, default=0)  # in hours
+    overtime_weekend = models.DecimalField(max_digits=10, decimal_places=2, default=0)  # in hours
+    rework_time = models.DecimalField(max_digits=10, decimal_places=2, default=0)  # in hours
+
+    class Meta:
+        abstract = True
+
+
+class InnovationStats(BaseStats):
     """
     Personal Performance status for QI team
     """
-    human_resource = models.ForeignKey(HumanResource)
-    analysis_hours = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    story_points_worked = models.PositiveIntegerField(default=0)
+    # Throughput
+    story_points_execution = models.PositiveIntegerField(default=0)
+    unit_tests_dev = models.PositiveIntegerField(default=0)
+    defects_in_dev = models.PositiveIntegerField(default=0)
+    elicitation_analysis_time = models.DecimalField(max_digits=10, decimal_places=2, default=0)  # in hours
+    revisions = models.PositiveIntegerField(default=0)
+    active_projects = models.PositiveIntegerField(default=0)
+
+    # Quality
+    uat_defects_not_prevented = models.PositiveIntegerField(default=0)
 
 
-class TestLabStats(models.Model):
+class LabStats(BaseStats):
     """
     Personal Performance status for TL team
     """
-    human_resource = models.ForeignKey(HumanResource)
+    # Throughput
+    tickets_received = models.PositiveIntegerField(default=0)
     tickets_closed = models.PositiveIntegerField(default=0)
 
 
-class RequirementStats(models.Model):
+class RequirementStats(BaseStats):
     """
     Personal Performance status for RE team
     """
-    human_resource = models.ForeignKey(HumanResource)
+    # Throughput
+    active_projects = models.PositiveIntegerField(default=0)
+    elicitation_analysis_time = models.DecimalField(max_digits=10, decimal_places=2, default=0)  # in hours
+
+    # Quality
+    revisions = models.PositiveIntegerField(default=0)
+
+    # Efficiency
+    rework_external_time = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+
+    # Costs
+    travel_cost = models.DecimalField(max_digits=10, decimal_places=2, default=0)
 
 
-class TestStats(models.Model):
+class TestStats(BaseStats):
     """
     Personal Performance status for PQ, QA, TE team
     """
-    human_resource = models.ForeignKey(HumanResource)
-
+    # Throughput
+    ticket_execution = models.PositiveIntegerField(default=0)
+    ticket_closed = models.PositiveIntegerField(default=0)
+    project_execution = models.PositiveIntegerField(default=0)
+    project_closed = models.PositiveIntegerField(default=0)
     tc_manual_dev = models.PositiveIntegerField(default=0)
+    tc_manual_dev_time = models.DecimalField(max_digits=10, decimal_places=2, default=0)  # in hours
     tc_manual_execution = models.PositiveIntegerField(default=0)
+    tc_manual_execution_time = models.DecimalField(max_digits=10, decimal_places=2, default=0,)  # in hours
     tc_auto_dev = models.PositiveIntegerField(default=0)
+    tc_auto_dev_time = models.DecimalField(max_digits=10, decimal_places=2, default=0)  # in hours
     tc_auto_execution = models.PositiveIntegerField(default=0)
+    tc_auto_execution_time = models.DecimalField(max_digits=10, decimal_places=2, default=0)  # in hours
 
-    tickets_worked = models.PositiveIntegerField(default=0)
-    initiatives_worked_hours = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-
-
+    # Quality
+    defect_caught = models.PositiveIntegerField(default=0)
+    uat_defects_not_prevented = models.PositiveIntegerField(default=0)
+    standards_violated = models.PositiveIntegerField(default=0)
 
 
