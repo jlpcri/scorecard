328e62f1612851aa2d66fb005a20fc47bcf1e7ea
Songqing Liu <sliu@west.com>
2016-01-14 10:07:45 -0600

    update pre-defined date range picker etc.

--- scorecard/apps/teams/templates/teams/teams.js
+++ scorecard/apps/teams/templates/teams/teams.js
@@ -13,11 +13,9 @@ function attachDateRangePicker() {
     $('#date-range').daterangepicker({
         ranges: {
             'Today': [moment(), moment()],
-            'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-            'Last 7 Days': [moment().subtract(6, 'days'), moment()],
             'Last 30 Days': [moment().subtract(29, 'days'), moment()],
-            'This Month': [moment().startOf('month'), moment().endOf('month')],
-            'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+            'Last 60 Days': [moment().subtract(59, 'days'), moment()],
+            'This Year': [moment().startOf('year'), moment().endOf('year')]
         }
     }, setDate(start, end));
 }
--- scorecard/apps/teams/utils.py
+++ scorecard/apps/teams/utils.py
@@ -21,17 +21,17 @@ def context_teams(request):
     functional_groups = FunctionalGroup.objects.all()
     for functional_group in functional_groups:
         if functional_group.key == 'PQ':
-            pqs = functional_group.testmetrics_set.filter(created__range=(start, end)).order_by('created')
+            pqs = functional_group.testmetrics_set.filter(created__range=(start, end)).order_by('-created')
         elif functional_group.key == 'QA':
-            qas = functional_group.testmetrics_set.filter(created__range=(start, end)).order_by('created')
+            qas = functional_group.testmetrics_set.filter(created__range=(start, end)).order_by('-created')
         elif functional_group.key == 'QI':
-            qis = functional_group.innovationmetrics_set.filter(created__range=(start, end)).order_by('created')
+            qis = functional_group.innovationmetrics_set.filter(created__range=(start, end)).order_by('-created')
         elif functional_group.key == 'RE':
-            res = functional_group.requirementmetrics_set.filter(created__range=(start, end)).order_by('created')
+            res = functional_group.requirementmetrics_set.filter(created__range=(start, end)).order_by('-created')
         elif functional_group.key == 'TE':
-            tes = functional_group.testmetrics_set.filter(created__range=(start, end)).order_by('created')
+            tes = functional_group.testmetrics_set.filter(created__range=(start, end)).order_by('-created')
         elif functional_group.key == 'TL':
-            tls = functional_group.labmetrics_set.filter(created__range=(start, end)).order_by('created')
+            tls = functional_group.labmetrics_set.filter(created__range=(start, end)).order_by('-created')
 
     context = {
         'pqs': pqs,
