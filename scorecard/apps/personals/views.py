41fc3bb6c436f50cbd550ab8486c271d1da9df37
Songqing Liu <sliu@west.com>
2016-01-15 09:43:06 -0600

    fix personal stats list cannot display records with two digits day number

--- scorecard/apps/personals/views.py
+++ scorecard/apps/personals/views.py
@@ -210,7 +210,7 @@ def fetch_personals_by_date(request):
 def fetch_personals_per_team_per_date(key, date):
     year = date[:4]
     month = date[6:7]
-    day = date[9:10]
+    day = date[8:10]
     data = []
 
     if key in ['PQ', 'QA', 'TE']:
