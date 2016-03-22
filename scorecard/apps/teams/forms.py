f4c48e810e6db1f16d344f21c797cf24ce3a7323
Songqing Liu <sliu@west.com>
2016-03-22 10:32:49 -0500

    fix Innovation/Test form unit tests error

--- scorecard/apps/teams/forms.py
+++ scorecard/apps/teams/forms.py
@@ -21,7 +21,7 @@ class InnovationForm(ModelForm):
 
     class Meta:
         model = InnovationMetrics
-        exclude = ['created', 'confirmed', 'updated', 'functional_group']
+        exclude = ['created', 'confirmed', 'updated', 'functional_group', 'subteam']
 
 
 class LabForm(ModelForm):
@@ -79,7 +79,7 @@ class TestForm(ModelForm):
 
     class Meta:
         model = TestMetrics
-        exclude = ['created', 'confirmed', 'updated', 'functional_group']
+        exclude = ['created', 'confirmed', 'updated', 'functional_group', 'subteam']
         # widgets = {
         #     'functional_group': forms.Select(attrs={
         #         'class': 'form-control',
