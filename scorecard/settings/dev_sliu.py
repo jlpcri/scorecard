8784ff7826d4e7381eb1d7bb4f0685bd1cce6300
Songqing Liu <sliu@west.com>
2016-04-04 16:47:42 -0500

    Add db settings on linux6437

--- scorecard/settings/dev_sliu.py
+++ scorecard/settings/dev_sliu.py
@@ -13,14 +13,26 @@ HOST_URL = 'http://10.6.20.106:8000' + LOGIN_URL
 # HOST_URL = 'http://apps.qaci01.wic.west.com' + LOGIN_URL
 
 # if socket.gethostname() == 'sliu-OptiPlex-GX520':
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#             'NAME': 'scorecard_subteamed',
-#             'USER': 'scorecard',
-#             'PASSWORD': 'scorecard_development',
-#             'HOST': 'qaci01.wic.west.com',
-#             # 'PORT': '5432',
-#             'PORT': '5433'  # another postgres instance
-#         }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'scorecard_subteamed',
+        'USER': 'scorecard',
+        'PASSWORD': 'scorecard_development',
+        'HOST': 'qaci01.wic.west.com',
+        # 'PORT': '5432',
+        'PORT': '5433'  # another postgres instance
+    }
+}
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'scorecard',
+#         'USER': 'visilog',
+#         'PASSWORD': '6ewuON0>;wHTe(DttOwjg#5NY)U497xKVwOxmQt60A1%}r:@qC&`7OdSP8u[.l[',
+#         'HOST': 'linux6437.wic.west.com',
+#         'PORT': '5432'
 #     }
+# }
