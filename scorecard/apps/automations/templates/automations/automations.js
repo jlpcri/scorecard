0f4a0e0b312ab21e6835c60676d5dd89727f7d0b
Songqing Liu <sliu@west.com>
2016-05-25 13:31:29 -0500

    Update help message on automatoin modal etc.

--- scorecard/apps/automations/templates/automations/automations.js
+++ scorecard/apps/automations/templates/automations/automations.js
@@ -1,19 +1,42 @@
 /**
  * Created by sliu on 2/15/16.
  */
-$('#automation-new-modal form').submit(function(event){
-    var file = $('#id_script_file')[0].files[0];
-    if (file){
-        if (!file.name.match('.py')){
-            $('#newAutomationErrMessage').html('UploadFile is not .py');
-            return false;
-        }
-    }
-});
 
 $(document).ready(function(){
     var role = $('#user_role').val();
     if (role == 'normal') {
         $('a[href="#personal"]').click();
+    }
+});
+
+$('.newAutomationTeam form').submit(function(event){
+    var file = $('.newAutomationTeam form #id_script_file')[0].files[0];
+    if (file){
+        if (!file.name.match('.py')){
+            showErrMsg('#newAutomationTeamErrMessage', 'UploadFile is not .py');
+            return false;
+        }
+    }
+});
+
+$('.newAutomationPersonal form').submit(function(event){
+    var file = $('.newAutomationPersonal form #id_script_file')[0].files[0];
+    if (file){
+        if (!file.name.match('.py')){
+            showErrMsg('#newAutomationPersonalErrMessage', 'UploadFile is not .py');
+            return false;
+        }
+    }
+});
+
+$('.batchAutomationPersonal form').submit(function(event){
+    var file = $('.batchAutomationPersonal form #id_script_file')[0].files[0];
+    console.log(file);
+    if (typeof file == 'undefined'){
+        showErrMsg('#batchAutomationPersonalErrMessage', 'No upload file selected.');
+        return false;
+    } else if (!file.name.match('.py')){
+        showErrMsg('#batchAutomationPersonalErrMessage', 'UploadFile is not .py');
+        return false;
     }
 });
